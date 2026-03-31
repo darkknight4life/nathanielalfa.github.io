ee5ba6cf9a3e39edf8dcff249337c8132da80270
nathaniel alfa putra <nathanielalfa3@gmail.com>
2026-03-31 10:23:14 +0700

    Add files via upload

---
+++ tugas biodata/script.js
@@ -0,0 +1,9 @@
+function toggleSidebar() {
+document.getElementById("sidebar").classList.toggle("active");
+}
+
+function scrollContact(){
+document.getElementById("contact").scrollIntoView({
+behavior:"smooth"
+});
+}
